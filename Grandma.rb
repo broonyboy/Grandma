eaa9accd0527c7222f15b328ad0d6fcf6a519593
Neale Brown <nbrown3@sky.com>
2017-06-07 20:26:31 +0100

    end of part one.

--- Grandma.rb
+++ Grandma.rb
@@ -3,11 +3,12 @@ bye = false
 while bye == false
     puts "What would you like to say to Grandma?"
     say = gets.chomp
-    if (say.upcase == "BYE") 
-    end
-    else if( say == say.upcase)
+    if (say.upcase == "BYE")
+        break
+    elsif( say == say.upcase)
     ranNum = rand(20) + 1930
     puts "NO, NOT SINCE " + ranNum.to_s + "!"
     else
     puts "HUH! SPEAK UP SONNY!"
+    end
 end
