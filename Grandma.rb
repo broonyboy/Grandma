6d339166188d11e4e75f406a555c7e43dd0e1107
Neale Brown <nbrown3@sky.com>
2017-06-07 20:36:09 +0100

    final commit added saying bye three times.

--- Grandma.rb
+++ Grandma.rb
@@ -1,14 +1,20 @@
 bye = false
+buyNumber = []
 
 while bye == false
     puts "What would you like to say to Grandma?"
     say = gets.chomp
     if (say.upcase == "BYE")
-        break
+        buyNumber.push(1)
+        if buyNumber.length == 3
+            break
+        end
     elsif( say == say.upcase)
     ranNum = rand(20) + 1930
     puts "NO, NOT SINCE " + ranNum.to_s + "!"
+        buyNumber = []
     else
     puts "HUH! SPEAK UP SONNY!"
+            buyNumber = []
     end
 end
